217592bacd2784f2490da14c599ac2225910e3fe
sai <sairamreddy2211@gmail.com>
2024-12-02 00:15:14 +0530

    tailwind setup and unnecessary files removal

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,28 @@
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function NotFoundScreen() {
   return (
-    <>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
       <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
+      <Text>This screen doesnt exist</Text>
+      <View>
+      <Text  className="text-red-500">Hello, World!</Text>
+      <Image
+        source={require('../assets/images/business_decode_logo.png')}
+            resizeMode="contain"
+            className='mt-10 w-[100px] h-[100px]'
+            //  style={{width:100, height:100}}
+          />
+    </View>
+      <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
+            Log in to Aora fuck you likki fuck u fuck u fuck u mee thatadengai
+            mee mama
+          </Text>
+     
+    </ScrollView>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
